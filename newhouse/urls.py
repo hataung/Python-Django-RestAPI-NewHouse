6a15c679036fd2af701acf4a9695535de0328371
William <william@masscode-nl.home>
2024-04-01 09:56:25 +0200

    My First Commit

---
+++ newhouse/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for newhouse project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('superhouse/', admin.site.urls),
+    path('api/v1/auth/', include("djoser.urls")),
+    path('api/v1/auth/', include("djoser.urls.jwt")),
+    path('api/v1/profile/', include("apps.profiles.urls")),
+    path("api/v1/properties/", include("apps.properties.urls")),
+    path("api/v1/ratings/", include("apps.ratings.urls")),
+    path("api/v1/enquiries/", include("apps.enquiries.urls")),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "New House Real Estate Admin"
+admin.site.site_title = "New House Admin Portal"
+admin.site.index_title = "Welcome to New House Portal"
